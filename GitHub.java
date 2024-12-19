8e758c54b614f71d6e5839acbd2b6a4c10aafd02
SaahilT13 <saahil.thambinayagam26@ycdsbk12.ca>
2024-12-19 09:15:15 -0500

    Test

---
+++ GitHub.java
@@ -0,0 +1,10 @@
+import arc.*;
+
+public class githubtest {
+    public static void main(String[] args) {
+        Console con = new Console();
+        
+        con.println("Hello World");
+        
+    }
+}
